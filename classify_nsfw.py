d4b5f0df445c0ce3883a0270bec728725ba8559d
toosyou <toosyou@mip1070.mip1070.nctucs.net>
2016-12-11 01:52:50 +0800

    add init function for gpu_server

--- classify_nsfw.py
+++ classify_nsfw.py
@@ -79,6 +79,22 @@ def caffe_preprocess_and_compute(pimg, caffe_transformer=None, caffe_net=None,
     else:
         return []
 
+def init_model(model_def, pretrained_model):
+    # Pre-load caffe model.
+    nsfw_net = caffe.Net(model_def,  # pylint: disable=invalid-name
+        pretrained_model, caffe.TEST)
+
+    # Load transformer
+    # Note that the parameters are hard-coded for best results
+    caffe_transformer = caffe.io.Transformer({'data': nsfw_net.blobs['data'].data.shape})
+    caffe_transformer.set_transpose('data', (2, 0, 1))  # move image channels to outermost
+    caffe_transformer.set_mean('data', np.array([104, 117, 123]))  # subtract the dataset-mean value in each channel
+    caffe_transformer.set_raw_scale('data', 255)  # rescale from [0, 1] to [0, 255]
+    caffe_transformer.set_channel_swap('data', (2, 1, 0))  # swap channels from RGB to BGR
+
+    return nsfw_net, caffe_transformer
+
+
 
 def main(argv):
     pycaffe_dir = os.path.dirname(__file__)
@@ -116,7 +132,10 @@ def main(argv):
     # scores[1] indicates the NSFW probability
     while True:
         # Classify.
-        image_data = raw_input()
+        number_bytes = input()
+        image_data = os.read( sys.stdin, number_bytes )
+        with open('test.jpg', 'wb') as out_jpg:
+            out_jpg.write(image_data)
         scores = caffe_preprocess_and_compute(image_data, caffe_transformer=caffe_transformer, caffe_net=nsfw_net, output_layers=['prob'])
         print scores[1]
 
